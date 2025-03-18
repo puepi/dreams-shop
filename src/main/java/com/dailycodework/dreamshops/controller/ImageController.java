e5038a7f566c314ae0fb61335c653b51841c9547
puepi <pmandel0001@yahoo.fr>
2025-03-18 03:45:24 +0100

    implemented download image method within ImageController

--- src/main/java/com/dailycodework/dreamshops/controller/ImageController.java
+++ src/main/java/com/dailycodework/dreamshops/controller/ImageController.java
@@ -2,15 +2,21 @@ package com.dailycodework.dreamshops.controller;
 
 
 import com.dailycodework.dreamshops.dto.ImageDto;
+import com.dailycodework.dreamshops.model.Image;
 import com.dailycodework.dreamshops.response.ApiResponse;
 import com.dailycodework.dreamshops.service.image.IImageService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.HttpServerErrorException;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.sql.SQLException;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -29,5 +35,16 @@ public class ImageController {
         }
     }
 
-    
+    @GetMapping("image/download/{imageId}")
+    public ResponseEntity<Resource> downloadImage(@PathVariable Long imageId){
+        Image image=imageService.getImageById(imageId);
+        try {
+            ByteArrayResource resource=new ByteArrayResource(image.getImage().getBytes(1,(int)image.getImage().length()));
+            return ResponseEntity.ok().contentType(MediaType.parseMediaType(image.getFileType()))
+                    .header(HttpHeaders.CONTENT_DISPOSITION,"attachment: filename=\"" +image.getFileName() +"\"")
+                    .body(resource);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
