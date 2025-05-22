ab5c2003957af577833c0945d43993573761a042
puepi <pmandel0001@yahoo.fr>
2025-05-22 09:33:40 +0100

    made one more change and still ran successfully

--- src/main/java/com/dailycodework/dreamshops/service/product/IProductService.java
+++ src/main/java/com/dailycodework/dreamshops/service/product/IProductService.java
@@ -3,7 +3,6 @@ package com.dailycodework.dreamshops.service.product;
 import com.dailycodework.dreamshops.dto.ProductDto;
 import com.dailycodework.dreamshops.model.Product;
 import com.dailycodework.dreamshops.request.AddProductRequest;
-import com.dailycodework.dreamshops.request.ProductUpdateRequest;
 
 import java.util.List;
 
